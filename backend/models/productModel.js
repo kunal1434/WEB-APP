18ab32bd350c34322bd6111edf3657489aa2b92d
kunal1434 <127184451+kunal1434@users.noreply.github.com>
2025-07-29 20:15:19 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,7 +5,7 @@ const reviewSchema = new mongoose.Schema(
     name: { type: String, required: true },
     rating: { type: Number, default: 0 },
     comment: { type: String, required: true },
-  },
+   },
   {
     timestamps: true,
   }
